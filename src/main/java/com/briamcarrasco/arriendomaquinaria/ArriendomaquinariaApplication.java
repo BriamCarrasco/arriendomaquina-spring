51cb1e1b0e76c098dcaf23335901474627c1df5b
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-10-24 19:04:38 -0300

    Archivos iniciales

---
+++ src/main/java/com/briamcarrasco/arriendomaquinaria/ArriendomaquinariaApplication.java
@@ -0,0 +1,13 @@
+package com.briamcarrasco.arriendomaquinaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArriendomaquinariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArriendomaquinariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/briamcarrasco/arriendomaquinaria/ArriendomaquinariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.briamcarrasco.arriendomaquinaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArriendomaquinariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
