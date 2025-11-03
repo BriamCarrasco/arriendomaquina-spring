2da61c7268740c3fcede4227b25b8746aed10004
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-11-03 04:23:34 -0300

    se generan entidades

--- src/main/java/com/briamcarrasco/arriendomaquinaria/model/Machinery.java
+++ src/main/java/com/briamcarrasco/arriendomaquinaria/model/Machinery.java
@@ -1,22 +1,33 @@
 package com.briamcarrasco.arriendomaquinaria.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 import java.math.BigDecimal;
 
 @Entity
-@Table(name = "maquinaria") //tabla en MySQL
+@Table(name = "tb_machinery") 
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
 public class Machinery {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
-    private String nombre;
-    private String categoria;
-    private String estado;
+    @Column(name = "name_machinery", nullable = false)
+    @NotBlank
+    private String nameMachinery;
 
-    @Column(name = "precio_dia", precision = 12, scale = 2)
-    private BigDecimal precioDia;
+    @Column(name = "category", nullable = false)
+    @NotBlank
+    private String category;
+
+    @Column(name = "status", nullable = false)
+    @NotBlank
+    private String status;
+
+    @Column(name = "price_per_day", precision = 12, scale = 2, nullable = false)
+    @NotBlank
+    private BigDecimal pricePerDay;
 }
--- src/main/java/com/briamcarrasco/arriendomaquinaria/model/UserInfo.java
+++ src/main/java/com/briamcarrasco/arriendomaquinaria/model/UserInfo.java
@@ -1,5 +1,48 @@
 package com.briamcarrasco.arriendomaquinaria.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+
+@Data
+@Entity
+@Table (name = "tb_usersinfo")
 public class UserInfo {
-    
+
+    @Id
+    @Column(name = "id_userinfo", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name", nullable = false, length = 50)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 50)
+    private String lastName;
+
+    @Column(name = "phone_number", nullable = false, length = 15)
+    private String phoneNumber;
+
+    @Column(name = "address", nullable = false, length = 100)
+    private String address;
+
+    @Column(name = "city", nullable = false, length = 50)
+    private String city;
+
+    @Column(name = "country", nullable = false, length = 50)
+    private String country;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    User user;
+
+    public UserInfo() {
+    }
 }
