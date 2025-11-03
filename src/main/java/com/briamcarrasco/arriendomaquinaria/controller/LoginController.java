079fa35c618e473c1b45ffe31b145e6d99ae9497
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-11-03 04:27:17 -0300

    se modifica get de login

--- src/main/java/com/briamcarrasco/arriendomaquinaria/controller/LoginController.java
+++ src/main/java/com/briamcarrasco/arriendomaquinaria/controller/LoginController.java
@@ -1,14 +1,16 @@
 package com.briamcarrasco.arriendomaquinaria.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class LoginController {
 
+
     @GetMapping("/login")
-    public String login() {
+    public String showLogin() {
         return "login";
     }
-    
+
+
 }
