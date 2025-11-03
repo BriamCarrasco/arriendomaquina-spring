2e16b987003250d4cdac55c68cbcdddad8e77b01
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-11-03 16:33:33 -0300

    se genera metodo createUser en userService para generar usuarios de test en aplicacion

--- src/main/java/com/briamcarrasco/arriendomaquinaria/service/UserService.java
+++ src/main/java/com/briamcarrasco/arriendomaquinaria/service/UserService.java
@@ -1,5 +1,26 @@
 package com.briamcarrasco.arriendomaquinaria.service;
 
+import com.briamcarrasco.arriendomaquinaria.model.User;
+import com.briamcarrasco.arriendomaquinaria.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
-    
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User createUser(String username, String password, String email, User.Role role) {
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(passwordEncoder.encode(password));
+        user.setEmail(email);
+        user.setRole(role);
+        return userRepository.save(user);
+    }
 }
