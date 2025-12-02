b54771c64ddb9dcdf565ee60c3de5963ee4fa0e4
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-12-01 21:22:54 -0300

    Se simplifica logica de clave jwt

--- src/main/java/com/briamcarrasco/arriendomaquinaria/jwt/Constants.java
+++ src/main/java/com/briamcarrasco/arriendomaquinaria/jwt/Constants.java
@@ -17,35 +17,16 @@ public class Constants {
     }
 
     public static final String REDIRECT_USERS = "redirect:/api/admin/users";
-
-    // Spring Security
     public static final String LOGIN_URL = "/login";
     public static final String HEADER_AUTHORIZACION_KEY = "Authorization";
     public static final String TOKEN_BEARER_PREFIX = "Bearer ";
-
-    // JWT
     public static final String ISSUER_INFO = "https://www.duocuc.cl/";
     public static final long TOKEN_EXPIRATION_TIME = 1000L * 60 * 60 * 24; // 24h
-    public static final String SUPER_SECRET_KEY = getEnvSecret();
-
+    public static final String JWT_SECRET_KEY = "XDWDH3u4tdxebqBp2Iesst01kCurg10QegORWr3igpt"; // Hardcodeada
     public static final String DUPLICATE_MEDIA_ERROR = "duplicate_media";
     public static final String ERROR_KEY = "error";
     public static final String MESSAGE_KEY = "message";
     public static final String UNIQUE_CONSTRAINT_ERROR_MSG = "No se puede agregar media: posible restricción única en la base de datos";
-
-    /**
-     * Obtiene la clave secreta JWT desde las variables de entorno.
-     *
-     * @return clave secreta en formato String
-     * @throws IllegalStateException si la variable de entorno no está definida
-     */
-    private static String getEnvSecret() {
-        String key = System.getenv("JWT_SECRET_KEY");
-        if (key == null || key.isEmpty()) {
-            throw new IllegalStateException("JWT_SECRET_KEY no está definida en las variables de entorno.");
-        }
-        return key;
-    }
 
     /**
      * Decodifica la clave secreta en base64 y retorna el objeto SecretKey para
